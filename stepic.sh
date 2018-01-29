4b440aa094ec2eeafe356d74279ae2f9cebdf7b2
ph0en <ph0en@users.noreply.github.com>
2018-01-30 02:21:02 +0600

    Create stepic.sh

---
+++ stepic.sh
@@ -0,0 +1,29 @@
+mkdir -p /home/box/web/{public,etc,uploads}
+mkdir  /home/box/web/public/{img,css,js}
+chown -R box:box /home/box/web/
+
+sed -i 's/user www-data/user box/g' /etc/nginx/nginx.conf
+rm /etc/nginx/sites-enabled/default
+> /home/box/web/etc/site.conf
+ln -s /home/web/etc/site.conf /etc/nginx/sites-enabled/site.conf
+
+cat << EOF >> /etc/nginx/sites-enabled/site.conf
+server {
+	listen 80;
+	server_name _;
+
+	location ^~ /uploads/ {
+		root /home/box/web/;
+	}
+
+	location ~ \.\w\w\w?\w?$ {
+		root /home/box/web/public;
+	}
+
+	location / {
+		return 404;
+	}
+}
+EOF
+
+nginx -t && service nginx start
